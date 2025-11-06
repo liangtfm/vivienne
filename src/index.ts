cfd5eb61323be0acc30102c7cdc3d27141c67211
Anthony Liang <liangtfm@gmail.com>
2025-11-05 22:52:57 -0800

    fix: change title

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.get("/", (c) => {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>3D Spinning Heart</title>
+  <title>Vivienne Liang</title>
   <style>
     * {
       margin: 0;
